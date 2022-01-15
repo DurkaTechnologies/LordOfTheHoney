ab6fb612b04f74a3c87b269a0b5c985ff24f6f00
Kolya Koval <kovalkola2@gmail.com>
2022-01-16 00:59:37 +0200

    Code decoration. World generation outside

--- WebUI/ClientApp/src/components/game/libs/initialize.ts
+++ WebUI/ClientApp/src/components/game/libs/initialize.ts
@@ -13,6 +13,7 @@ import { Quaternion, Vector2, Vector3 } from "three";
 import { VoxelLandscape } from "./3dLib/VoxelWorld/VoxelLandscape";
 
 import { mapFromCannon, mapFromThree } from "./mapperVector";
+import { WorldGenerator } from "./3dLib/VoxelWorld/WorldGenerator";
 
 export class InitializeGame {
   initialize: () => void;
@@ -20,12 +21,14 @@ export class InitializeGame {
   initCannon: () => void;
   initControls: () => void;
   render: () => void;
-  updateBoxesFromVoxels: () => void;
   switchHeader: (data: boolean) => void;
+  getGeneratingBlockDirection: () => CANNON.Vec3 | undefined;
 
   world: CANNON.World;
   scene: THREE.Scene;
   renderer: THREE.WebGLRenderer;
+  worldGenerator: WorldGenerator;
+  primitives: Primitives;
 
   cubeHelperMesh: THREE.Mesh | null = null;
   cubeHelperBody: CANNON.Body | null = null;
@@ -38,13 +41,13 @@ export class InitializeGame {
   sphereBody: CANNON.Body;
   physicsMaterial: CANNON.Material;
 
-  primitives: Primitives;
-  voxels: VoxelLandscape;
-
   time = Date.now();
 
   boxMeshes: Array<THREE.Mesh<THREE.BoxGeometry, THREE.MeshLambertMaterial>> =
     [];
+  boxes: Array<CANNON.Body> = [];
+
+  isRayLineShow = true;
 
   lastCallTime = performance.now() / 1000;
 
@@ -56,17 +59,6 @@ export class InitializeGame {
 
   // groundBody: CANNON.Body;
   defaultMaterial: THREE.MeshLambertMaterial;
-
-  boxes: Array<CANNON.Body> = [];
-
-  voxelWorld: VoxelWorld;
-
-  nx = 50;
-  ny = 8;
-  nz = 50;
-  sx = 1;
-  sy = 1;
-  sz = 1;
 
   constructor(switchHeader: (data: boolean) => void) {
     this.switchHeader = switchHeader;
@@ -87,27 +79,6 @@ export class InitializeGame {
     this.defaultMaterial = new THREE.MeshLambertMaterial({ color: 0xdddddd });
 
     this.primitives = new Primitives(this.world, this.scene);
-
-    this.voxels = new VoxelLandscape(
-      this.world,
-      this.nx,
-      this.ny,
-      this.nz,
-      this.sx,
-      this.sy,
-      this.sz
-    );
-
-    const cellSize = 50;
-    const tileSize = 16;
-    const tileTextureWidth = 256;
-    const tileTextureHeight = 64;
-    this.voxelWorld = new VoxelWorld({
-      cellSize,
-      tileSize,
-      tileTextureWidth,
-      tileTextureHeight,
-    });
 
     const canvas = document.querySelector("#webgl");
     this.renderer = new THREE.WebGLRenderer({ canvas: canvas as Element });
@@ -134,31 +105,9 @@ export class InitializeGame {
 
       this.world.step(1 / 60);
 
-      for (let i = 0; i < this.voxels.boxes.length; i++) {
-        this.boxMeshes[i].position.copy(this.voxels.boxes[i].position);
-        this.boxMeshes[i].quaternion.copy(this.voxels.boxes[i].quaternion);
-      }
-      this.camera.getWorldPosition(this.positionVector);
-      this.camera.getWorldDirection(this.lookAtVector);
-
-      this.lookAtVector.multiplyScalar(5);
-      this.lookAtVector.addVectors(this.lookAtVector, this.positionVector);
-
-      const ray = new CANNON.Ray(
-        mapFromThree(this.positionVector),
-        mapFromThree(this.lookAtVector)
-      );
-      ray.mode = CANNON.RAY_MODES.CLOSEST;
-      const result = new CANNON.RaycastResult();
-      ray.intersectBodies(this.boxes, result);
-
-      if (result.body?.position && result.hasHit) {
-        let resultVector = new CANNON.Vec3();
-        resultVector = result.body.vectorToWorldFrame(result.hitPointWorld);
-        resultVector.x = Math.floor(resultVector.x) + 0.5;
-        resultVector.z = Math.floor(resultVector.z) + 0.5;
-        resultVector.y = Math.floor(resultVector.y) + 0.5;
-        const box = this.primitives.createCube(resultVector, true);
+      const direction = this.getGeneratingBlockDirection();
+      if (direction) {
+        const box = this.primitives.createCube(direction, true);
         this.cubeHelperBody = box.body;
         this.cubeHelperMesh = box.mesh;
       }
@@ -189,6 +138,8 @@ export class InitializeGame {
     texture.magFilter = THREE.NearestFilter;
     texture.minFilter = THREE.NearestFilter;
 
+    this.worldGenerator = new WorldGenerator(this.world, this.scene, texture);
+
     this.initializeThree = () => {
       const sizes = {
         width: window.innerWidth,
@@ -210,13 +161,6 @@ export class InitializeGame {
       //   cellSize * 0.8,
       //   -cellSize * 0.3
       // );
-
-      // const controls = new OrbitControls(
-      //   camera,
-      //   document.getElementById("webgl")!
-      // );
-      // controls.target.set(cellSize / 2, cellSize / 3, cellSize / 2);
-      // controls.update();
 
       resizeEvent(sizes, this.camera, this.renderer);
       // dblClickEvent(canvas);
@@ -241,29 +185,6 @@ export class InitializeGame {
 
       let renderRequested: boolean | undefined = false;
 
-      const height = 1;
-
-      this.render;
-
-      // for (let y = 0; y < cellSize; ++y) {
-      //   for (let z = 0; z < cellSize; ++z) {
-      //     for (let x = 0; x < cellSize; ++x) {
-      //       if (y < height) {
-      //         console.log("VECTOR:", new Vector3(x, y, z));
-      //         world.setVoxel(x, y, z, randInt(1, 17), this.primitives, true);
-      //       }
-      //     }
-      //   }
-      // }
-      // console.log("COUNTER: ", counter);
-
-      // const materialA = new THREE.MeshLambertMaterial({ color: 0xdddddd });
-      // const floorGeometry = new THREE.PlaneBufferGeometry(300, 300, 100, 100);
-      // floorGeometry.rotateX(-Math.PI / 2);
-      // this.floor = new THREE.Mesh(floorGeometry, materialA);
-      // this.floor.receiveShadow = true;
-      // this.scene.add(this.floor);
-
       function randInt(min: number, max: number) {
         return Math.floor(Math.random() * (max - min) + min);
       }
@@ -287,43 +208,25 @@ export class InitializeGame {
       };
 
       const stayBlock = () => {
-        this.camera.getWorldPosition(this.positionVector);
-        this.camera.getWorldDirection(this.lookAtVector);
-
-        this.lookAtVector.multiplyScalar(5);
-        this.lookAtVector.addVectors(this.lookAtVector, this.positionVector);
-
-        const ray = new CANNON.Ray(
-          mapFromThree(this.positionVector),
-          mapFromThree(this.lookAtVector)
-        );
-        ray.mode = CANNON.RAY_MODES.CLOSEST;
-        const result = new CANNON.RaycastResult();
-        ray.intersectBodies(this.boxes, result);
-        // console.log("result.hitFaceIndex: ", result.hitFaceIndex);
-
-        if (result.body?.position && result.hasHit) {
-          let resultVector = new CANNON.Vec3();
-          resultVector = result.body.vectorToWorldFrame(result.hitPointWorld);
-          resultVector.x = Math.floor(resultVector.x) + 0.5;
-          resultVector.z = Math.floor(resultVector.z) + 0.5;
-          resultVector.y = Math.floor(resultVector.y) + 0.5;
-          console.log("resultVector: ", resultVector);
-          const box = this.primitives.createCube(resultVector).body;
+        const direction = this.getGeneratingBlockDirection();
+        if (direction) {
+          const box = this.primitives.createCube(direction).body;
           this.boxes.push(box);
         }
 
-        // const materialLine = new THREE.LineBasicMaterial({ color: 0x0000ff });
+        if (this.isRayLineShow) {
+          const materialLine = new THREE.LineBasicMaterial({ color: 0x0000ff });
 
-        // const points = [];
-        // points.push(this.positionVector);
-        // points.push(this.lookAtVector);
+          const points = [];
+          points.push(this.positionVector);
+          points.push(this.lookAtVector);
 
-        // const geometry = new THREE.BufferGeometry().setFromPoints(points);
+          const geometry = new THREE.BufferGeometry().setFromPoints(points);
 
-        // const line = new THREE.Line(geometry, materialLine);
+          const line = new THREE.Line(geometry, materialLine);
 
-        // this.scene.add(line);
+          this.scene.add(line);
+        }
       };
 
       // controls.addEventListener("change", requestRenderIfNotRequested);
@@ -367,93 +270,12 @@ export class InitializeGame {
       const radius = 1.3;
       sphereShape = new CANNON.Sphere(radius);
       this.sphereBody.addShape(sphereShape);
-      this.sphereBody.position.set(
-        this.nx * this.sx * 0.5,
-        this.ny * this.sy + radius * 2,
-        this.nz * this.sz * 0.5
-      );
+      this.sphereBody.position.set(1, 18.6, 1);
       this.sphereBody.linearDamping = 0.9;
       this.world.addBody(this.sphereBody);
 
-      for (let i = 0; i < this.nx; i++) {
-        for (let j = 0; j < this.ny; j++) {
-          for (let k = 0; k < this.nz; k++) {
-            let filled = true;
-
-            // Insert map constructing logic here
-            if (Math.sin(i * 0.1) * Math.sin(k * 0.1) < (j / this.ny) * 2 - 1) {
-              filled = false;
-            }
-
-            this.voxels.setFilled(i, j, k, filled);
-            if (filled) {
-              this.voxelWorld.setVoxel(i, j, k, 14);
-            }
-          }
-        }
-      }
-
-      this.voxels.update();
-
-      console.log(`${this.voxels.boxes.length} voxel physics bodies`);
-
-      // Voxel meshes
-      for (let i = 0; i < this.voxels.boxes.length; i++) {
-        const box = this.voxels.boxes[i];
-
-        const voxelGeometry = new THREE.BoxBufferGeometry(
-          this.voxels.sx * box.nx,
-          this.voxels.sy * box.ny,
-          this.voxels.sz * box.nz
-        );
-        const voxelMesh = new THREE.Mesh(voxelGeometry, this.defaultMaterial);
-        voxelMesh.castShadow = true;
-        voxelMesh.receiveShadow = true;
-        this.boxMeshes.push(voxelMesh);
-        // this.scene.add(voxelMesh);
-      }
-
-      const { positions, normals, uvs, indices } =
-        this.voxelWorld.generateGeometryDataForCell(0, 0, 0);
-      const geometry = new THREE.BufferGeometry();
-      const material = new THREE.MeshLambertMaterial({
-        map: texture,
-        side: THREE.DoubleSide,
-        alphaTest: 0.1,
-        transparent: true,
-      });
-
-      const positionNumComponents = 3;
-      const normalNumComponents = 3;
-      const uvNumComponents = 2;
-      geometry.setAttribute(
-        "position",
-        new THREE.BufferAttribute(
-          new Float32Array(positions),
-          positionNumComponents
-        )
-      );
-      geometry.setAttribute(
-        "normal",
-        new THREE.BufferAttribute(
-          new Float32Array(normals),
-          normalNumComponents
-        )
-      );
-      geometry.setAttribute(
-        "uv",
-        new THREE.BufferAttribute(new Float32Array(uvs), uvNumComponents)
-      );
-      geometry.setIndex(indices);
-      const mesh = new THREE.Mesh(geometry, material);
-      this.scene.add(mesh);
-
-      this.updateBoxesFromVoxels();
-
-      // const groundShape = new CANNON.Plane();
-      // this.groundBody.addShape(groundShape);
-      // this.groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
-      // this.world.addBody(this.groundBody);
+      this.worldGenerator.worldGenerate();
+      this.boxes = [...this.worldGenerator.getBoxes()];
     };
     this.initControls = () => {
       this.controls = new PointerLockControlsCannon(
@@ -476,9 +298,30 @@ export class InitializeGame {
         this.controls.enabled = false;
       });
     };
-    this.updateBoxesFromVoxels = () => {
-      this.boxes = [...this.voxels.getBoxes()];
-      console.log("[...this.voxels.getBoxes()]: ", [...this.voxels.getBoxes()]);
+    this.getGeneratingBlockDirection = () => {
+      this.camera.getWorldPosition(this.positionVector);
+      this.camera.getWorldDirection(this.lookAtVector);
+
+      this.lookAtVector.multiplyScalar(5);
+      this.lookAtVector.addVectors(this.lookAtVector, this.positionVector);
+
+      const ray = new CANNON.Ray(
+        mapFromThree(this.positionVector),
+        mapFromThree(this.lookAtVector)
+      );
+      ray.mode = CANNON.RAY_MODES.CLOSEST;
+      const result = new CANNON.RaycastResult();
+      ray.intersectBodies(this.boxes, result);
+      // console.log("result: ", result);
+
+      if (result.body?.position && result.hasHit) {
+        let resultVector = new CANNON.Vec3();
+        resultVector = result.body.vectorToWorldFrame(result.hitPointWorld);
+        resultVector.x = Math.floor(resultVector.x) + 0.5;
+        resultVector.z = Math.floor(resultVector.z) + 0.5;
+        resultVector.y = Math.floor(resultVector.y) + 0.5;
+        return resultVector;
+      }
     };
     this.initialize = () => {
       this.initializeThree();
